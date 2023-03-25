8dc3a18ded116161913d675d94733535d3f81e6e
elvis-visi <sivisevi1@gmail.com>
2023-03-25 13:51:36 +0000

    1.Album class -> album table.

---
+++ DatabaseSQLMusicApp/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSQLMusicApp
+{
+    
+    internal class Album
+    {
+        public int ID { get; set; } 
+        public string AlbumName { get; set; }               
+        public string ArtistName { get;}
+        public int Year { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+    }
+}
