c50582d7b4177f927dcd785cc5c6a3bf56c1dc81
elvis-visi <sivisevi1@gmail.com>
2023-03-28 18:56:07 +0100

    6.2 Tracks entity with properties corresponding to tracks table in music DB.

---
+++ DatabaseSQLMusicApp/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseSQLMusicApp
+{
+    internal class Track
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }    
+        public int Number { get; set; }
+
+        public string videoURL { get; set; }
+
+        public string lyrics { get; set; }
+
+    }
+}
